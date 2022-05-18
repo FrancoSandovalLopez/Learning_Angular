6e17b26578c057aaef846a7e03e8d890c95806b9
unknown <>
2022-05-18 08:06:24 -0400

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { MaterialModule } from './material/material.module';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/views/products/products.component.ts
@@ -0,0 +1,28 @@
+import { Product } from './interfaces/product.interface';
+import { ProductsService } from './services/products.service';
+import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  products!:Product[]
+
+  constructor(private productSvc: ProductsService) { }
+
+  ngOnInit(): void {
+    this.productSvc.getProducts()
+    .pipe(
+      tap( (products: Product[]) => this.products = products)
+    )
+    .subscribe();
+  }
+
+  addToCart(product:Product): void{
+    console.log('Add To Cart', product);
+  }
+}
