ce0f657ba039126d87f833ac56e4684a39c937f4
unknown <>
2022-06-02 22:52:25 -0400

    Adding new branch and commiting login routing and login/register pages (need some performance fixes})

--- src/app/views/account/account-routing.module.ts
+++ src/app/views/account/account-routing.module.ts
@@ -1,16 +1,22 @@
+import { LayoutLoginComponent } from './layout-login/layout-login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
+    component: LayoutLoginComponent,
     children: [
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent }
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      }
     ]
   }
 ];
