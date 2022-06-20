71309168a227f025a83a402c6fe20af7aac7d48b
Franco Sandoval <fsandoval@munimacul.cl>
2022-06-20 10:52:45 -0400

    Added Register services and some performances improvement

--- src/app/views/account/auth/auth.service.ts
+++ src/app/views/account/auth/auth.service.ts
@@ -9,48 +9,48 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 const helper = new JwtHelperService();
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class AuthService {
   private loggedId = new BehaviorSubject<boolean>(false);
-  private headers = {'content-type': 'application/json'}
+  private headers = { 'content-type': 'application/json' };
 
   constructor(private http: HttpClient) {
     this.checkToken();
   }
 
-get isLgged(): Observable<boolean>{
-  return this.loggedId.asObservable();
-}
-
-  login(authData: User):Observable<UserResponse | void>{
-
-    const body = {
-      email: authData.email,
-      password: authData.password
-    }
-
-    return this.http
-    .post<any>(`${environment.API_URL}/auth/login`, JSON.stringify(body), {'headers': this.headers})
-    .pipe(
-      map( (res:UserResponse) => {
-        //console.log('RES ->', res);
-        this.saveToken(res.token);
-        this.loggedId.next(true);
-        return res;
-      }),
-      catchError((err) => this.handleError(err))
-    );
+  get isLgged(): Observable<boolean> {
+    return this.loggedId.asObservable();
   }
 
-  logout():void{
+  login(authData: User): Observable<UserResponse | void> {
+    const body = {
+      email: authData.email,
+      password: authData.password,
+    };
+
+    return this.http
+      .post<any>(`${environment.API_URL}/auth/login`, JSON.stringify(body), {
+        headers: this.headers,
+      })
+      .pipe(
+        map((res: UserResponse) => {
+          //console.log('RES ->', res);
+          this.saveToken(res.token);
+          this.loggedId.next(true);
+          return res;
+        }),
+        catchError((err) => this.handleError(err))
+      );
+  }
+
+  logout(): void {
     localStorage.removeItem('token');
     this.loggedId.next(false);
     //set userLogged = false;
   }
 
-  private checkToken(): void{
+  private checkToken(): void {
     const userToken = JSON.parse(localStorage.getItem('token') || '{}');
     /*
      * IF ISTOKENEXPIRED() RETURN TRUE
@@ -61,15 +61,14 @@ get isLgged(): Observable<boolean>{
     isExpired ? this.logout() : this.loggedId.next(true);
   }
 
-  private saveToken(token: string): void{
+  private saveToken(token: string): void {
     localStorage.setItem('token', token);
   }
 
-  private handleError(err: any): Observable<never>{
-   let errorMessage = 'An error ocurrend while retrienving data';
-   if(err)
-    errorMessage = `Error: Code ${err.message} `;
+  private handleError(err: any): Observable<never> {
+    let errorMessage = 'An error ocurrend while retrienving data';
+    if (err) errorMessage = `Error: Code ${err.message} `;
     window.alert(errorMessage);
-    return throwError(errorMessage)
+    return throwError(errorMessage);
   }
 }
